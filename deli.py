02e976495ce7e19033ec2752ff747dd48c4de7be
Xavier Chitison <chitisonx@gmail.com>
2026-09-11 11:42:12 -0400

    Adding logic

--- deli.py
+++ deli.py
@@ -8,4 +8,11 @@ sandwich_orders = [
     "roast beef"
 ]
 
-finished_sandwiches = []
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop()
+
+    print(f"I made your {current_sandwich} sandwich.")
+
+    finished_sandwiches.append(current_sandwich)
+
+print("\nFinished sandwiches:")
